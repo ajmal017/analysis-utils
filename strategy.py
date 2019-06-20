caf504019267e8e660021d20681fe76d621234c2
Jason Lai <laikasin93@yahoo.com.hk>
2019-06-20 17:43:15 +0800

    Add files via upload

---
+++ strategy.py
@@ -0,0 +1,45 @@
+# python strategy.py --backtest --start 2015-01-01 --end 2015-12-31 --data ~/mycsvdata/ --output ~/portfolio.pkl
+from qtpylib.algo import Algo
+
+class DumbAlgo(Algo):
+
+    def on_start(self):
+        # optional method that gets called once upon start
+        pass
+
+    def on_fill(self, instrument, order):
+        # optional method that gets called on every order fill
+        pass
+
+    def on_orderbook(self, instrument):
+        # optional method that gets called on every orderbook change
+        pass
+
+    def on_quote(self, instrument):
+        # optional method that gets called on every quote change
+        pass
+
+    def on_tick(self, instrument):
+        # optional method that gets called on every tick received
+        pass
+
+    def on_bar(self, instrument):
+        # optional method that gets called on every bar received
+
+        # buy if position = 0, sell if in position > 0
+        if instrument.positions['position'] == 0:
+            instrument.buy(100)
+        else:
+            instrument.exit()
+
+
+if __name__ == "__main__":
+
+    # initialize the algo
+    strategy = DumbAlgo(
+        instruments = [ ("0001.HK", "STK", "SEHK", "HKD") ],
+        resolution  = "1D" # 1Min bar resolution (Pandas "resample" resolutions)
+    )
+
+    # run the algo
+    strategy.run()
